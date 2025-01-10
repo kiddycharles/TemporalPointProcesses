04716b11f2921a7003438b4ca98ba92fa029acf6
Charles Ye <31942142+kiddycharles@users.noreply.github.com>
2025-01-11 07:50:32 +0800

    Update Layers.py
    FeedForward Network in the encoder layer is extrapositioned

--- Mine/transformer/Layers.py
+++ Mine/transformer/Layers.py
@@ -10,16 +10,16 @@ class EncoderLayer(nn.Module):
         super(EncoderLayer, self).__init__()
         self.slf_attn = MultiHeadAttention(
             n_head, d_model, d_k, d_v, dropout=dropout, normalize_before=normalize_before)
-        self.pos_ffn = PositionwiseFeedForward(
-            d_model, d_inner, dropout=dropout, normalize_before=normalize_before)
+        # self.pos_ffn = PositionwiseFeedForward(
+        #     d_model, d_inner, dropout=dropout, normalize_before=normalize_before)
 
     def forward(self, enc_input, non_pad_mask=None, slf_attn_mask=None):
         enc_output, enc_slf_attn = self.slf_attn(
             enc_input, enc_input, enc_input, mask=slf_attn_mask)
         enc_output *= non_pad_mask
 
-        enc_output = self.pos_ffn(enc_output)
-        enc_output *= non_pad_mask
+        # enc_output = self.pos_ffn(enc_output)
+        # enc_output *= non_pad_mask
 
         return enc_output, enc_slf_attn
 
